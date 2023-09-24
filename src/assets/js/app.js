019d3f7b77d2ddb5b9926db002be31c8d825e49f
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-09-24 20:10:38 +0200

    Add Order block

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,24 @@
 fun = (a) => {
-    return a;
+   return a;
+}
+
+
+
+const decrementButton = document.getElementById('decrement');
+const incrementButton = document.getElementById('increment');
+const counterElement = document.getElementById('counter');
+let counterValue = 0;
+
+decrementButton.addEventListener('click', () => {
+   counterValue--;
+   updateCounter();
+});
+
+incrementButton.addEventListener('click', () => {
+   counterValue++;
+   updateCounter();
+});
+
+function updateCounter() {
+   counterElement.textContent = counterValue;
 }
