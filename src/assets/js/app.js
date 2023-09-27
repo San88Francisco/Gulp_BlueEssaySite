75db4bb76e8fd6617d85d5f47ab79fdd5c83a646
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-09-27 16:56:06 +0200

    05

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,3 +22,29 @@ incrementButton.addEventListener('click', () => {
 function updateCounter() {
    counterElement.textContent = counterValue;
 }
+
+
+
+
+
+const block = document.getElementById("block");
+
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const offsetX = 10;
+
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    const deltaX = mouseX - centerX;
+    const deltaY = mouseY - centerY;
+
+    const angle = Math.atan2(deltaY, deltaX);
+
+    const translateX = offsetX * Math.cos(angle);
+    const translateY = offsetX * Math.sin(angle);
+
+    block.style.transform = `translate(${translateX}px, ${translateY}px)`;
+});
