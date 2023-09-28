9bb07384ccad7ef6db3759fb33d773883213ac2d
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2023-09-28 16:51:23 +0200

    all

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -30,21 +30,21 @@ function updateCounter() {
 const block = document.getElementById("block");
 
 document.addEventListener("mousemove", (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+   const mouseX = e.clientX;
+   const mouseY = e.clientY;
 
-    const offsetX = 10;
+   const offsetX = 10;
 
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
+   const centerX = window.innerWidth / 2;
+   const centerY = window.innerHeight / 2;
 
-    const deltaX = mouseX - centerX;
-    const deltaY = mouseY - centerY;
+   const deltaX = mouseX - centerX;
+   const deltaY = mouseY - centerY;
 
-    const angle = Math.atan2(deltaY, deltaX);
+   const angle = Math.atan2(deltaY, deltaX);
 
-    const translateX = offsetX * Math.cos(angle);
-    const translateY = offsetX * Math.sin(angle);
+   const translateX = offsetX * Math.cos(angle);
+   const translateY = offsetX * Math.sin(angle);
 
-    block.style.transform = `translate(${translateX}px, ${translateY}px)`;
+   block.style.transform = `translate(${translateX}px, ${translateY}px)`;
 });
